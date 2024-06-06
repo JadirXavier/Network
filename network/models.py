f6a31f10893f3ba4e24c76a22be62e18ea918c40
jadir1 <joxf1994@gmail.com>
2024-06-06 19:24:40 -0300

    Changed post view layout

---
+++ network/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    
+    def __str__(self):
+        return f"{self.username} - {self.email}"
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_user")
+    body = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} posted at {self.timestamp}"
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="like_user")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="like_post")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} liked at {self.timestamp}"
+
+class Follow (models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followed")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.follower} followed {self.follower}"
