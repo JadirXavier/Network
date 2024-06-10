460f16dcc6108cb80d89390a2cff912f302a3299
Jadir Xavier <49170436+JadirXavier@users.noreply.github.com>
2024-06-10 19:52:16 +0000

    Made following/followers functionallity

--- network/views.py
+++ network/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -21,7 +22,6 @@ def index(request):
     return render(request, "network/index.html", {
         "posts": posts
     })
-
 
 def login_view(request):
     if request.method == "POST":
@@ -73,3 +73,40 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+def profile(request, username):
+    posts = Post.objects.filter(user__username=username).order_by('-timestamp')
+    user_profile = User.objects.get(username=username)
+    user = request.user
+    followers_count = Follow.objects.filter(followed=user_profile).count()
+    following_count = Follow.objects.filter(follower=user_profile).count()
+    follows = Follow.objects.filter(follower=user, followed=user_profile)
+    return render(request, "network/profile.html", {
+        "user_profile":user_profile,
+        "posts":posts,
+        "user":user,
+        "follows":follows,
+        "following_count":following_count,
+        "followers_count":followers_count,
+    })
+
+@login_required
+def follow(request, username):
+    user_to_follow = User.objects.get(username=username)
+    user_following = request.user
+
+    if user_to_follow != user_following:
+        Follow.objects.create(follower=user_following, followed=user_to_follow)
+
+    return redirect('profile', username=username)
+
+@login_required
+def unfollow(request, username):
+    user_to_follow = User.objects.get(username=username)
+    user_following = request.user
+    follow = Follow.objects.filter(follower=user_following, followed=user_to_follow)
+    if user_to_follow != user_following:
+        if follow.exists():
+            follow.delete()
+
+    return redirect('profile', username=username)
