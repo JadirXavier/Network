50d44937b6dc187992870ab1508c3c76578f6316
Jadir Xavier <49170436+JadirXavier@users.noreply.github.com>
2024-06-12 19:53:10 +0000

    Made so edit button can hide post view and show edit view

--- network/static/network/scripts.js
+++ network/static/network/scripts.js
@@ -1,3 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Seleciona todos os botões de edição
+  const editButtons = document.querySelectorAll('.edit-button');
+  
+  // Adiciona um event listener a cada botão de edição
+  editButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Obtém o ID do post a partir do atributo data-post-id
+      const postId = button.getAttribute('data-post-id');
+      const postViewId = `#post_view_${postId}`;
+      const postEditId = `#post_edit_${postId}`;
+      
+      // Chama a função edit_post com os IDs apropriados
+      edit_post(postEditId, postViewId);
+    });
+  });
+});
+
+
+
+// Pagination
 function goToPage(maxPage) {
   let input = document.getElementById("page_input");
   let page = parseInt(input.value);
@@ -8,3 +29,15 @@ function goToPage(maxPage) {
       alert("Please enter a valid page number between 1 and " + maxPage);
   }
 }
+
+  //Hide post_view and show post_edit
+  function edit_post(edit, post) {
+    const editElement = document.querySelector(edit);
+    const postElement = document.querySelector(post);
+    
+    editElement.className = 'card m-3 border rounded d-flex flex-column'
+    editElement.style.display = 'block';
+    postElement.className = ''
+    postElement.style.display = 'none';
+
+  }
